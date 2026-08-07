3a0190b34c01cf5f7b3fb61773d6f03b2d6bfb86
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2026-08-06 20:46:03 -0400

    Network Port WIP

---
+++ DEBUG/debug_main.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+
+const PLAYER_CONTROLLER = preload("res://Scenes/Player.tscn")
+var players: Array[CharacterBody3D]
+@onready var spawn_point = $PLAYER_SPAWN
+@onready var multiplayer_ui = $ConnectionMenu
+
+func _on_host_pressed():
+	
+	NetworkManager.host_lobby()
+	
+	multiplayer.peer_connected.connect(
+		func(pid):
+			print("Peer " + str(pid) + " has joined the game!")
+			add_player(pid)
+	)
+	add_player(multiplayer.get_unique_id())
+	multiplayer_ui.hide()
+
+
+func _on_join_pressed():
+	
+	match NetworkManager.backend_type:
+		NetworkManager.BACKEND_TYPES.LAN:
+			NetworkManager.join_lan_lobby()
+	
+	add_player(multiplayer.get_unique_id())
+	multiplayer_ui.hide()
+
+
+func add_player(pid):
+	var player : CharacterBody3D = PLAYER_CONTROLLER.instantiate()
+	player.name = str(pid)
+	player.position = spawn_point.position
+	$PlayerSpawns/PlayerPuppets.add_child(player)
