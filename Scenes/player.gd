ae928cd252ba27fb2eb435f5e8d72dd136e3dd44
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2026-08-06 20:39:14 -0400

    Dual backend Script (I hope this works)

---
+++ Scenes/player.gd
@@ -0,0 +1,56 @@
+extends CharacterBody3D
+
+
+const WALK_SPEED := 5
+const JUMP_SPEED := 5
+
+@onready var camera : Camera3D = $Head/Camera3D
+@onready var head: Node3D = $Head
+
+
+@export var mouse_sensistivity = 0.5
+
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(int(name.to_int()))
+
+
+func _input(event):
+	if !is_multiplayer_authority():
+		return
+	
+	#Camera movement 
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x) * mouse_sensistivity)
+		head.rotate_x(deg_to_rad(-event.relative.y) * mouse_sensistivity)
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-60),deg_to_rad(50))
+	
+
+
+func _physics_process(delta: float) -> void:
+	camera.current = is_multiplayer_authority()
+	
+	if !is_multiplayer_authority():
+		return
+	
+	#Movement code that is rotated to be relative to the camera position 
+	var movement = Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction = Vector3(movement.x, 0, movement.y).rotated(Vector3.UP, head.global_rotation.y).normalized()
+	
+	
+	if(movement):
+		velocity.x = direction.x * WALK_SPEED
+		velocity.z = direction.z * WALK_SPEED
+	else:
+		velocity.x = 0
+		velocity.z = 0
+	
+	#Jumping
+	if(Input.is_action_just_pressed("Jump") and is_on_floor()):
+		velocity.y = JUMP_SPEED
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	move_and_slide()
--- Singletons/NetworkManager.gd
+++ Singletons/NetworkManager.gd
@@ -1,11 +1,97 @@
+## Most Networking code should go here! 
 extends Node
 
+## When this client creates a lobby 
+signal host_created()
+## When this client joins a lobby 
+signal joined_lobby()
 
-# Called when the node enters the scene tree for the first time.
+
+## The peer of the client 
+var peer : MultiplayerPeer 
+
+
+
+## Backend Selection
+enum BACKEND_TYPES {STEAM, LAN}
+var backend_type : BACKEND_TYPES = BACKEND_TYPES.LAN
+
+const MAX_MEMBERS := 4
+
+## Lan Info
+const LAN_PORT = 25565
+## Steam Info
+const STEAM_LOBBY_TYPE := Steam.LobbyType.LOBBY_TYPE_FRIENDS_ONLY
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	## Setup Steam singals for Steam Backend
+	Steam.initRelayNetworkAccess()
+	Steam.lobby_created.connect(on_steam_lobby_created)
+	Steam.lobby_joined.connect(on_steam_lobby_joined)
+	Steam.join_requested.connect(on_steam_join_requested)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+## Hosts a Lobby from either backend 
+func host_lobby() -> void:
+	match backend_type:
+		BACKEND_TYPES.STEAM:
+			Steam.createLobby(STEAM_LOBBY_TYPE, MAX_MEMBERS)
+		BACKEND_TYPES.LAN:
+			peer = ENetMultiplayerPeer.new()
+			peer.create_server(LAN_PORT, MAX_MEMBERS)
+			multiplayer.multiplayer_peer = peer
+			host_created.emit()
+
+
+#region LAN Pipeline 
+func join_lan_lobby():
+	match backend_type:
+		BACKEND_TYPES.LAN:
+			peer = ENetMultiplayerPeer.new()
+			peer.create_client("localhost", LAN_PORT)
+			multiplayer.multiplayer_peer = peer
+			joined_lobby.emit()
+#endregion
+
+
+
+#region SteamAPI
+
+# Called after creating a lobby locally
+func on_steam_lobby_created(connect: int, lobby_id: int) -> void:
+	match backend_type:
+		BACKEND_TYPES.STEAM:
+			# We created the lobby, so we act as server host
+			if connect == Steam.RESULT_OK:
+				peer = SteamMultiplayerPeer.new()
+				peer.server_relay = true
+				peer.create_host()
+				multiplayer.multiplayer_peer = peer
+				host_created.emit()
+
+# Called when joining a lobby (after creating the lobby or joining a friend)
+func on_steam_lobby_joined(lobby_id: int, permissions: int, locked: bool, response: int) -> void:
+	match backend_type:
+		BACKEND_TYPES.STEAM:
+			if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
+				# If we created the lobby, we are already hosting, so we should not create a new client peer
+				if Steam.getLobbyOwner(lobby_id) == Steam.getSteamID():
+					return
+				peer = SteamMultiplayerPeer.new()
+				peer.server_relay = true
+				peer.create_client(Steam.getLobbyOwner(lobby_id))
+				multiplayer.multiplayer_peer = peer
+				joined_lobby.emit()
+
+# Called when attempting to join from the Steam interface
+func on_steam_join_requested(lobby_id: int, steam_id: int) -> void:
+	
+	match backend_type:
+		BACKEND_TYPES.STEAM:
+			# Will cause the "lobby_joined" signal to emit
+			Steam.joinLobby(lobby_id)
+
+#endregion 
